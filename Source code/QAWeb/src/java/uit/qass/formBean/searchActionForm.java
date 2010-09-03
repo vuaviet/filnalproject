adae1334b129fd0e53043596c1fc8c4cb921a4c5
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-09-03 04:06:50 +0000

    commit search pages

---
+++ Source code/QAWeb/src/java/uit/qass/formBean/searchActionForm.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.qass.formBean;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.util.ImageButtonBean;
+
+/**
+ *
+ * @author Hoang-PC
+ */
+public class searchActionForm extends org.apache.struts.action.ActionForm {
+
+    private String type;
+    private String keyWord;
+    private ImageButtonBean submit;
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public ImageButtonBean getSubmit() {
+        return submit;
+    }
+
+    public void setSubmit(ImageButtonBean submit) {
+        this.submit = submit;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    @Override
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        if (getKeyWord() == null || "".equals(getKeyWord())) {
+            errors.add("key word", new ActionMessage("errors.required"));
+            return errors;
+        }
+        if (getKeyWord().length() < 5) {
+            errors.add("key word", new ActionMessage("errors.minlength"));
+            errors.add("5", new ActionMessage("errors.minlength"));
+            return errors;
+        }
+        return errors;
+    }
+}
--- Source code/QAWeb/web/public/layouts/qadb-1/common/menu.jsp
+++ Source code/QAWeb/web/public/layouts/qadb-1/common/menu.jsp
@@ -11,7 +11,7 @@
 <td class="bg3" id ="menu_bg3" >
     <ul>
         <li><a href="#" class="link1">Home</a></li>
-        <li><a href="#" class="link1">Search</a></li>
+        <li><a href="searchPage.do" class="link1">Search</a></li>
         <li><a href="#" class="link1">Advance Search</a></li>
         <li><a href="#" class="link1">Contact us</a></li>
     </ul>
