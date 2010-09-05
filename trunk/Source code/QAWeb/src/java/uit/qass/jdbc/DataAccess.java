08d977aa90a9380775325c06d3f8be7c18c0cfea
hungnt.it@gmail.com <hungnt.it@gmail.com@579e1918-7e80-e334-f938-c355d7de1f55>
2010-09-05 17:41:45 +0000

    lib cho viec truy xuat database

---
+++ trunk/Source code/QAWeb/src/java/uit/qass/jdbc/DataAccess.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package uit.qass.jdbc;
+
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javax.sql.DataSource;
+import uit.qass.jndi.JNDIUtil;
+import uit.qass.util.InfrastructureUtil;
+import uit.qass.util.log.Log;
+import uit.qass.util.log.LogFactoryUtil;
+
+/**
+ * <a href="DataAccess.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class DataAccess {
+
+	public static Connection getConnection() throws SQLException {
+		DataSource ds = InfrastructureUtil.getDataSource();
+
+		return ds.getConnection();
+	}
+
+	public static Connection getConnection(String location)
+		throws NamingException, SQLException {
+
+		InitialContext ctx = new InitialContext();
+
+		DataSource ds = (DataSource)JNDIUtil.lookup(ctx, location);
+
+		return ds.getConnection();
+	}
+
+	public static void cleanUp(Connection connection) {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		catch (SQLException sqle) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(sqle.getMessage());
+			}
+		}
+	}
+
+	public static void cleanUp(Connection connection, Statement statement) {
+		cleanUp(statement);
+		cleanUp(connection);
+	}
+
+	public static void cleanUp(
+		Connection connection, Statement statement, ResultSet resultSet) {
+
+		cleanUp(resultSet);
+		cleanUp(statement);
+		cleanUp(connection);
+	}
+
+	public static void cleanUp(ResultSet resultSet) {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+		}
+		catch (SQLException sqle) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(sqle.getMessage());
+			}
+		}
+	}
+
+	public static void cleanUp(Statement statement) {
+		try {
+			if (statement != null) {
+				statement.close();
+			}
+		}
+		catch (SQLException sqle) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(sqle.getMessage());
+			}
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(DataAccess.class);
+
+}
