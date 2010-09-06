9c203e474edf977661882841f471622ae1269be8
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-09-06 05:26:36 +0000

    test file for custom query

---
+++ trunk/Source code/QAWeb/src/java/uit/qass/util/dao/orm/Test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uit.qass.util.dao.orm;
+
+import uit.qass.util.StringPool;
+
+/**
+ *
+ * @author aa
+ */
+public class Test {
+    public static void main(String args[]){
+        String[] keys = CustomSQLUtil.keywords("Philip K. Chan");
+        for(String s:keys){
+            System.out.println(s);
+        }
+        String sql = "select distinct author from dblp_author_ref_new where (dblp_author_ref_new.author LIKE ? [$AND_OR_NULL_CHECK$])";
+        String result = CustomSQLUtil.replaceKeywords(sql, "dblp_author_ref_new.author", StringPool.LIKE, true, keys);
+        String sql2     =   CustomSQLUtil.replaceAndOperator(result, true);
+        System.out.println(result);
+        System.out.println(sql2);
+    }
+}
