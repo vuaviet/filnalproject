7b3aaf6f256e3b770fd50e63cc39263ad3beb956
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-10-02 03:58:42 +0000

    - add new package to prepare for QA module. - create some empty classes.

---
+++ Source code/QAWeb/src/java/uit/qabpss/analysisandprocess/EntityReg.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uit.qabpss.analysisandprocess;
+
+/**
+ *
+ * @author aa
+ */
+public class EntityReg {
+
+}
---
+++ Source code/QAWeb/src/java/uit/qabpss/generatequery/GenSQLQuery.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uit.qabpss.generatequery;
+
+/**
+ *
+ * @author aa
+ */
+public class GenSQLQuery {
+
+}
