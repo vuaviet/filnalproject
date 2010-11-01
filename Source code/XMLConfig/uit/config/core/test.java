fab3412db695ee5304934505a20aee21960f6de5
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-01 03:51:10 +0000

    source interactive with xmlconfig

---
+++ Source code/XMLConfig/uit/config/core/test.java
@@ -0,0 +1,9 @@
+package uit.config.core;
+
+
+public class test {
+	public static void main(String args[]){
+		XMLReader rd = new XMLReader();
+		System.out.println(rd.relationWordMapping("write"));
+	}
+}
