1b1e7ee4762de7733e299b4bd079cdb02bd55d6b
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-02 04:29:28 +0000

    add file read XML

---
+++ Source code/QAWeb/src/java/uit/qabpss/dbconfig/XMLReader.java
@@ -0,0 +1,90 @@
+package uit.qabpss.dbconfig;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLReader {
+	private static final String REVERSED_RELATION = "reversed-relation";
+
+	private static final String FIELD = "field";
+
+	private static final String FIELD_ALIAS = "field-alias";
+
+	private static final String TABLE = "table";
+
+	private static final String TABLE_ALIAS = "table-alias";
+
+	private static final String RELATION_NAME = "relation-name";
+
+	private static final String PATH = "xmlconfig\\rel_config.xml";
+
+	public Document doc = null;
+
+	public XMLReader() {
+		try {
+			File file = new File(PATH);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			doc = db.parse(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String relationWordMapping(String word) {
+		String firstObj = null;
+		String secondObj = null;
+		boolean isReverse = false;
+		Node temp = null;
+		Node n = null;
+		doc.getDocumentElement().normalize();
+		NodeList nodeLst = doc.getElementsByTagName(RELATION_NAME);
+		for (int i = 0; i < nodeLst.getLength(); i++) {
+			n = nodeLst.item(i);
+			if (word.equals(n.getTextContent())) {
+				temp = n.getParentNode();
+			}
+		}
+		if (temp == null) {
+			isReverse = true;
+			nodeLst = doc.getElementsByTagName(REVERSED_RELATION);
+			for (int i = 0; i < nodeLst.getLength(); i++) {
+				n = nodeLst.item(i);
+				if (word.equals(n.getTextContent())) {
+					System.out.println(n.getTextContent());
+					temp = n.getParentNode();
+				}
+			}
+		}
+
+		if (temp == null) {
+			return null;
+		}
+
+		while (temp != null) {
+			if (TABLE.equals(temp.getNodeName())
+					&& temp.getAttributes().getNamedItem(TABLE_ALIAS) != null) {
+				firstObj = temp.getAttributes().getNamedItem(TABLE_ALIAS)
+						.getNodeValue();
+			}
+			if (FIELD.equals(temp.getNodeName())
+					&& temp.getAttributes().getNamedItem(FIELD_ALIAS) != null) {
+				secondObj = temp.getAttributes().getNamedItem(FIELD_ALIAS)
+						.getNodeValue();
+			}
+			temp = temp.getParentNode();
+		}
+		
+		if (!isReverse) {
+			return firstObj + "-" + word + "-" + secondObj;
+		} else {
+			return secondObj + "-" + word + "-" + firstObj;
+		}
+	}
+}
---
+++ Source code/QAWeb/src/java/uit/qabpss/dbconfig/testXMLReader.java
@@ -0,0 +1,9 @@
+package uit.qabpss.dbconfig;
+
+
+public class testXMLReader {
+	public static void main(String args[]){
+		XMLReader rd = new XMLReader();
+		System.out.println(rd.relationWordMapping("has writer"));
+	}
+}
