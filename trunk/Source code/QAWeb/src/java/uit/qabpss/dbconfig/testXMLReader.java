5e4870304a258bd579673d2bad07bb250c2e9271
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-04 12:28:58 +0000

    - XMLreader: change method use to find element value

--- trunk/Source code/QAWeb/src/java/uit/qabpss/dbconfig/testXMLReader.java
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/dbconfig/testXMLReader.java
@@ -5,6 +5,7 @@ public class testXMLReader {
 	public static void main(String args[]){
 		XMLReader rd = new XMLReader();
 		System.out.println(rd.relationWordMapping("be publish"));
-                System.out.println(rd.getDBTableName("publication"));
+                System.out.println(rd.getValueElement("publication",XMLReader.TABLENAME));
+                System.out.println(rd.getValueElement("publication",XMLReader.PRIMARYKEY));
 	}
 }
