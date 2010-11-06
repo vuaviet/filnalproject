fc7fef5c54f8dfcc0db12a936d8ff3ebaf1cabeb
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-06 06:22:54 +0000

    - XMLreader: load XML to DBconfig and change some value in DBInfo, ColumnInfo, ...

--- trunk/Source code/QAWeb/src/java/uit/qabpss/dbconfig/XMLReader.java
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/dbconfig/XMLReader.java
@@ -10,15 +10,21 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class XMLReader {
+    public static final String DATABASE = "database";
+    public static final String DATABASE_NAME = "database-name";
+    public static final String FIELD_NAME = "field-name";
     public static final String PRIMARYKEY = "primary-key";
+    public static final String RELATED_TABLE = "related-table";
+    public static final String REVERSED_RELATION = "reversed-relation";
     public static final String TABLE = "table";
     public static final String TABLENAME = "table-name";
     public static final String RELATION = "relation";
-    public static final String REVERSED_RELATION = "reversed-relation";
     public static final String FIELD = "field";
     public static final String FIELD_ALIAS = "field-alias";
     public static final String TABLE_ALIAS = "table-alias";
     public static final String RELATION_NAME = "relation-name";
+    public static final String TYPE = "type";
+    public static final String VISIBLE = "visible";
     private static final String PATH = "xmlconfig\\rel_config.xml";
     public Document doc = null;
 
@@ -33,81 +39,147 @@ public class XMLReader {
         }
     }
 
-    public String relationWordMapping(String word) {
-        String firstObj = null;
-        String secondObj = null;
-        String rel = null;
-        boolean isReverse = false;
-        Node temp = null;
-        Node n = null;
-        doc.getDocumentElement().normalize();
-        // get relation from tags relation name
-        NodeList nodeLst = doc.getElementsByTagName(RELATION_NAME);
+    public DBInfo loadDBInfo(){
+        DBInfo dbInfo = new DBInfo();
+        NodeList nodeLst = doc.getElementsByTagName(DATABASE).item(0).getChildNodes();
         for (int i = 0; i < nodeLst.getLength(); i++) {
-            n = nodeLst.item(i);
-            if (word.equals(n.getTextContent())) {
-                temp = n.getParentNode();
+            Node n = nodeLst.item(i);
+            if(DATABASE_NAME.equals(n.getNodeName())){
+                dbInfo.setName(n.getTextContent());
             }
-        }
-        // get reversed relation from tags relation name
-        if (temp == null) {
-            isReverse = true;
-            nodeLst = doc.getElementsByTagName(REVERSED_RELATION);
-            for (int i = 0; i < nodeLst.getLength(); i++) {
-                n = nodeLst.item(i);
-                if (word.equals(n.getTextContent())) {
-                    System.out.println(n.getTextContent());
-                    temp = n.getParentNode();
+            if(TABLE.equals(n.getNodeName())){                
+                NodeList tbChilds = n.getChildNodes();
+                TableInfo tbInf = new TableInfo();
+                tbInf.setAliasName(n.getAttributes().getNamedItem(TABLE_ALIAS).getNodeValue());
+                tbInf.setClassTable(null);
+                for (int j = 0; j < tbChilds.getLength(); j++) {
+                    Node temp = tbChilds.item(j);
+                    if(TABLENAME.equals(temp.getNodeName())){
+                        tbInf.setName(temp.getTextContent());
+                    }
+                    if(PRIMARYKEY.equals(temp.getNodeName())){
+                        tbInf.setPrimaryKey(temp.getTextContent());
+                    }
+                    if(FIELD.equals(temp.getNodeName())){
+                        ColumnInfo colInf = new ColumnInfo();
+                        NodeList childFields = temp.getChildNodes();
+                        colInf.setAliasName(temp.getAttributes().getNamedItem(FIELD_ALIAS).getNodeValue());
+                        colInf.setIsVisible(temp.getAttributes().getNamedItem(VISIBLE).getNodeValue());
+//                        colInf.setName(temp.getAttributes().getNamedItem(FIELD_NAME).getNodeValue());
+                        colInf.setType(temp.getAttributes().getNamedItem(TYPE).getNodeValue());
+                        colInf.setRelationType(temp.getAttributes().getNamedItem(RELATION).getNodeValue());
+                        for (int k = 0; k < childFields.getLength(); k++) {
+                            Node childField = childFields.item(k);
+                            if(FIELD_NAME.equals(childField.getNodeName())){
+                                colInf.setName(childField.getTextContent());
+                            }
+                            if(RELATION.equals(childField.getNodeName())){
+                                NodeList relList = childField.getChildNodes();                                
+                                for (int l = 0; l < relList.getLength(); l++) {
+                                    Node rel = relList.item(l);                                    
+                                    if(RELATION_NAME.equals(rel.getNodeName())){
+                                        Relation fieldRel = new Relation();
+                                        fieldRel.setRelationName(rel.getTextContent());
+                                        fieldRel.setType(RELATION_NAME);
+                                        colInf.addRelation(fieldRel);
+                                    }
+                                    if(REVERSED_RELATION.equals(rel.getNodeName())){
+                                        Relation fieldRel = new Relation();
+                                        fieldRel.setRelationName(rel.getTextContent());
+                                        fieldRel.setType(REVERSED_RELATION);
+                                        colInf.addRelation(fieldRel);
+                                    }
+                                    if(RELATED_TABLE.equals(rel.getNodeName())){
+                                        colInf.setRelatedTable(rel.getTextContent());
+                                    }                                                                      
+                                }
+                            }
+                        }
+                        tbInf.addColumn(colInf);
+                    }
                 }
+                dbInfo.addTable(tbInf);
             }
         }
-        // not found any relations
-        if (temp == null) {
-            return null;
-        }
-        // get first object, second object and their relationship
-        while (temp != null) {
-            if (TABLE.equals(temp.getNodeName())
-                    && temp.getAttributes().getNamedItem(TABLE_ALIAS) != null) {
-                firstObj = temp.getAttributes().getNamedItem(TABLE_ALIAS).getNodeValue();
-            }
-            if (FIELD.equals(temp.getNodeName())
-                    && temp.getAttributes().getNamedItem(FIELD_ALIAS) != null) {
-                secondObj = temp.getAttributes().getNamedItem(FIELD_ALIAS).getNodeValue();
-                rel = temp.getAttributes().getNamedItem(RELATION).getNodeValue();
-            }
-            temp = temp.getParentNode();
-        }
-
-        if (!isReverse) {
-            return firstObj + "-" + rel + "-" + secondObj;
-        } else {
-            return secondObj + "-" + rel + "-" + firstObj;
-        }
+        return dbInfo;
     }
+    
+//    public String relationWordMapping(String word) {
+//        String firstObj = null;
+//        String secondObj = null;
+//        String rel = null;
+//        boolean isReverse = false;
+//        Node temp = null;
+//        Node n = null;
+//        doc.getDocumentElement().normalize();
+//        // get relation from tags relation name
+//        NodeList nodeLst = doc.getElementsByTagName(RELATION_NAME);
+//        for (int i = 0; i < nodeLst.getLength(); i++) {
+//            n = nodeLst.item(i);
+//            if (word.equals(n.getTextContent())) {
+//                temp = n.getParentNode();
+//            }
+//        }
+//        // get reversed relation from tags relation name
+//        if (temp == null) {
+//            isReverse = true;
+//            nodeLst = doc.getElementsByTagName(REVERSED_RELATION);
+//            for (int i = 0; i < nodeLst.getLength(); i++) {
+//                n = nodeLst.item(i);
+//                if (word.equals(n.getTextContent())) {
+//                    System.out.println(n.getTextContent());
+//                    temp = n.getParentNode();
+//                }
+//            }
+//        }
+//        // not found any relations
+//        if (temp == null) {
+//            return null;
+//        }
+//        // get first object, second object and their relationship
+//        while (temp != null) {
+//            if (TABLE.equals(temp.getNodeName())
+//                    && temp.getAttributes().getNamedItem(TABLE_ALIAS) != null) {
+//                firstObj = temp.getAttributes().getNamedItem(TABLE_ALIAS).getNodeValue();
+//            }
+//            if (FIELD.equals(temp.getNodeName())
+//                    && temp.getAttributes().getNamedItem(FIELD_ALIAS) != null) {
+//                secondObj = temp.getAttributes().getNamedItem(FIELD_ALIAS).getNodeValue();
+//                rel = temp.getAttributes().getNamedItem(RELATION).getNodeValue();
+//            }
+//            temp = temp.getParentNode();
+//        }
+//
+//        if (!isReverse) {
+//            return firstObj + "-" + rel + "-" + secondObj;
+//        } else {
+//            return secondObj + "-" + rel + "-" + firstObj;
+//        }
+//    }
+//
+//    public String getValueElement(String aliasTableName,String tagName){
+//        String result = null;
+//        if(aliasTableName.isEmpty() || tagName.isEmpty()){
+//            return null;
+//        }
+//
+//        try{
+//        doc.getDocumentElement().normalize();
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//
+//        Node n = null;
+//        // get relation from tags relation name
+//        NodeList nodeLst = doc.getElementsByTagName(TABLE);
+//        for (int i = 0; i < nodeLst.getLength(); i++) {
+//            n = nodeLst.item(i);
+//            if (aliasTableName.equals(n.getAttributes().getNamedItem(TABLE_ALIAS).getNodeValue())) {
+//                result = n.getOwnerDocument().getElementsByTagName(tagName).item(0).getTextContent();
+//            }
+//        }
+//        return result;
+//    }
 
-    public String getValueElement(String aliasTableName,String tagName){
-        String result = null;
-        if(aliasTableName.isEmpty() || tagName.isEmpty()){
-            return null;
-        }
-        
-        try{
-        doc.getDocumentElement().normalize();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-        Node n = null;
-        // get relation from tags relation name
-        NodeList nodeLst = doc.getElementsByTagName(TABLE);
-        for (int i = 0; i < nodeLst.getLength(); i++) {
-            n = nodeLst.item(i);            
-            if (aliasTableName.equals(n.getAttributes().getNamedItem(TABLE_ALIAS).getNodeValue())) {
-                result = n.getOwnerDocument().getElementsByTagName(tagName).item(0).getTextContent();
-            }
-        }
-        return result;
-    }
     
 }
