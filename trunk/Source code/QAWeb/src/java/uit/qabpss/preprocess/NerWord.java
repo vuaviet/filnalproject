f9647f3e1b75bce2f4e1bf0a17963aee27354ab8
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-08 10:16:19 +0000

    - fix name package

---
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/preprocess/NerWord.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uit.qabpss.preprocess;
+
+/**
+ *
+ * @author ThuanHung
+ */
+public class NerWord {
+    private String  key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public NerWord(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+
+}
---
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/preprocess/Token.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.qabpss.preprocess;
+
+/**
+ *
+ * @author ThuanHung
+ */
+public class Token {
+
+    private String value;
+    private String pos_value;
+
+    /**
+     * Get the value of value
+     *
+     * @return the value of value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Set the value of value
+     *
+     * @param value new value of value
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Get the value of pos_value
+     *
+     * @return the value of pos_value
+     */
+    public String getPos_value() {
+        return pos_value;
+    }
+
+    /**
+     * Set the value of pos_value
+     *
+     * @param pos_value new value of pos_value
+     */
+    public void setPos_value(String pos_value) {
+        this.pos_value = pos_value;
+    }
+
+    public Token(String value, String pos_value) {
+        this.value = value;
+        this.pos_value = pos_value;
+    }
+
+    public Token(Token token) {
+        this.pos_value = token.pos_value;
+        this.value = token.value;
+    }
+}
