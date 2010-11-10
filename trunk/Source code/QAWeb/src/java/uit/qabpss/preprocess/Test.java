da03626d6d198da658bec1a0e229e326d418a041
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-10 16:31:19 +0000

    - update class RelationReg.java

--- trunk/Source code/QAWeb/src/java/uit/qabpss/preprocess/Test.java
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/preprocess/Test.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package uit.qadpss.preprocess;
+package uit.qabpss.preprocess;
 
 import java.io.IOException;
 
