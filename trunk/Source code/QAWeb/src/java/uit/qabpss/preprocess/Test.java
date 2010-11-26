bb92f5e9e17eaac2b52ebfd050159f17f65b4817
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-11-26 07:53:28 +0000

    - Update new test questions: now test from 1-33th sentences - keep all rules

--- trunk/Source code/QAWeb/src/java/uit/qabpss/preprocess/Test.java
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/preprocess/Test.java
@@ -19,26 +19,26 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
+        // List of test questions here
         String[] questions = new String[]{
-           "Which books were written by Rafiul Ahad and Amelia Carlson in 2010 ? ",
-           "Which books were written by Rafiul Ahad from 1999 to 2010 ?",
+            "Which books were written by Rafiul Ahad and Amelia Carlson in 2010 ? ",
+            "Which books were written by Rafiul Ahad from 1999 to 2010 ?",
             "Which books were published by O'Reilly  in 1999 ?",
             "How many papers were written by Rafiul Ahad ?",
             "Who write books in 1999 ?",
             "Who write books from 1999 to 2010 ?",
             "How many papers were written by Rafiul Ahad in 2010 ?",
-           "Who published books from 1999 to 2000 ?",
+            "Who published books from 1999 to 2000 ?",
             "Who published books in 1999 ?",
             "What are titles of books written by Marcus Thint ?",
             "What papers did Jennifer Widom write ?",
             "What books did Jennifer Widom write ?",
-            "Who is the author of  \"Working Models for Uncertain Data and ACM\" 1999" ,
-            "Who is the author of  \"Working Models for Uncertain Data and ACM\"" ,
+            "Who is the author of  \"Working Models for Uncertain Data and ACM\" 1999",
+            "Who is the author of  \"Working Models for Uncertain Data and ACM\"",
             "What book did Philip K. Chan write in 1999?",
             "What book did Philip K. Chan write from 1999 to 2000?",
             "What are the titles of the books published by O’reilly in 1999 ?",
-           "What composer wrote \" Java 2D Graphics\"",
+            "What composer wrote \" Java 2D Graphics\"",
             "What books has isbn 1-56592-484-3",
             "What books has doi 10.1145/360271.360274",
             "What composer wrote books from 1999 in ACM?",
@@ -48,23 +48,24 @@ public class Test {
             "How many publisher did \"Philip K. Chan\" cooperate with?",
             "What books were published by ACM or Springer in 2010?",
             "What publications have resulted from TREC?",
-             "What publications have resulted from TREC in 1999?",//fail test
-             "Which author write \"database\" in 1999",
-             "Which books did Philip K. Chan or Marcus Thint write in ACM ?",
-             "What book did Philip K. Chan write in 1999 from ACM?",
-             "Name some books Richard L. Muller write for Springer",
-             "List all books were published by Springer in 2010",
-             "What year is \"Foundations of Databases.\" written ?",// not solve yet
-             "What books refer to \"Foundations of Databases.\"",// not solve yet
+            "What publications have resulted from TREC in 1999?",//fail test
+            "Which author write \"Foundations of Databases.\" in 1999",
+            "Which books did Philip K. Chan or Marcus Thint write in ACM ?",
+            "What book did Philip K. Chan write in 1999 from ACM?",
+            "Name some books which Richard L. Muller writes for Springer",
+            "List all books were published by Springer in 2010",
+            "What year is \"Foundations of Databases.\" written ?",// not solve yet
+            "What books refer to \"Foundations of Databases.\"",// not solve yet
+            "What books did Richard L. Muller write for Springer",
         };
-        System.out.println("nums test: "+questions.length);
-        ExtractTriple extract   =   new ExtractTriple();
+        System.out.println("nums test: " + questions.length);
+        ExtractTriple extract = new ExtractTriple();
         for (String question : questions) {
             System.out.println("----------------------------------------------------------------------------");
             Token[] tokens = SentenseUtil.formatNerWordInQuestion(question);
             tokens = SentenseUtil.optimizePosTags(tokens);
             System.out.println(SentenseUtil.tokensToStr(tokens));
-            List<TripleToken> list  =   extract.extractTripleWordRelation(tokens);
+            List<TripleToken> list = extract.extractTripleWordRelation(tokens);
             for(TripleToken tripleToken:list)
             {
                 System.out.println(tripleToken);
