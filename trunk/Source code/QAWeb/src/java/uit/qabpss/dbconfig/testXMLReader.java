65a2c0e10a196844228d2a006a6f0e5d96270cf2
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-12-06 11:21:57 +0000

    - change news in XMLconfig : get Related Table get Mapping Table - ColumnInfo had two fields :related table and mapping table

--- trunk/Source code/QAWeb/src/java/uit/qabpss/dbconfig/testXMLReader.java
+++ trunk/Source code/QAWeb/src/java/uit/qabpss/dbconfig/testXMLReader.java
@@ -9,5 +9,16 @@ public class testXMLReader {
 //                System.out.println(rd.getValueElement("publication",XMLReader.PRIMARYKEY));
                 DBInfo mytestDBInfo = rd.loadDBInfo();
                 System.out.println(mytestDBInfo.getTables().get(0).getColumns().get(0).relation.get(0).getRelationName());
+
+                TableInfo tbTest = rd.getRelatedTable("author");
+                System.out.println(tbTest.getAliasName());
+                System.out.println(tbTest.getName());
+                System.out.println(tbTest.getPrimaryKey());
+                System.out.println("-------------------");
+                MappingTable mapTest = rd.getMappingTable("pub_and_pub");
+                System.out.println(mapTest.getName());
+                System.out.println(mapTest.getMappingTableName());
+                System.out.println(mapTest.getRelatedTableKey());
+                System.out.println(mapTest.getTableKey());
 	}
 }
