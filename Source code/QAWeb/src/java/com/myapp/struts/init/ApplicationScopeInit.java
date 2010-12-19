07d464f6df8706f824fdb23b25fce700fd1e5d7f
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2010-12-19 14:59:50 +0000

    - update QA SEARCH GUI (not finish)

--- Source code/QAWeb/src/java/com/myapp/struts/init/ApplicationScopeInit.java
+++ Source code/QAWeb/src/java/com/myapp/struts/init/ApplicationScopeInit.java
@@ -1,19 +1,16 @@
 package com.myapp.struts.init;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
 import org.apache.struts.util.LabelValueBean;
-import uit.qabpss.dbconfig.DBInfo;
+
 import uit.qabpss.dbconfig.DBInfoUtil;
 import uit.qabpss.util.hibernate.HibernateUtil;
 
@@ -31,6 +28,41 @@ public class ApplicationScopeInit implements ServletContextListener {
         try {
             HibernateUtil.getSessionFactory();
             DBInfoUtil.initDb();
+
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            //Use context classloader to read states.properties
+            InputStream iStream = loader.getResourceAsStream("uit/qabpss/extracttriple/SampleQuestion.properties");
+            Properties props = new Properties();
+            //Load the stream into the properties object directly
+            props.load(iStream);
+            //Look up by key and load them into a ArrayList as NameValuePair collection
+            Enumeration keyEnum = props.propertyNames();
+            // Use a Sorted Set to hold the state names and values
+            // Define an anonymous inner class to provide
+            // the comparison algorithm to the TreeSet
+            Set stateSet = new TreeSet(
+                    new Comparator() {
+                        public int compare(Object a, Object b) {
+                            LabelValueBean nvpA = (LabelValueBean) a;
+                            LabelValueBean nvpB = (LabelValueBean) b;
+
+                            String valA = nvpA.getLabel();
+                            String valB = nvpB.getLabel();
+                            return valA.compareTo(valB);
+                        }
+                    });
+            LabelValueBean nvp = null;
+            String keyName = null;
+            String label = null;
+            while (keyEnum.hasMoreElements()) {
+                keyName = (String) keyEnum.nextElement();
+                label = props.getProperty(keyName);
+                nvp = new LabelValueBean(label, keyName);
+                stateSet.add(nvp);
+            }
+            //Get ServletContext and set the properties as a application scope object
+            ServletContext context = event.getServletContext();
+            context.setAttribute("EXAMPLE_QUESTIONS", stateSet);
         } catch (Exception e) {
         }
     }
