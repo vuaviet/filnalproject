776095258b83253cd87c0fe87e4a92171b6deb3b
minhtuyen142 <minhtuyen142@579e1918-7e80-e334-f938-c355d7de1f55>
2011-01-04 01:55:48 +0000

    - delete un-use Source code/XMLConfig - update tasks : task doned (100%) - delete file update DBLP_SQL.txt ( already have in main source)

--- trunk/Source code/XMLConfig/uit/config/core/test.java
+++
@@ -1,9 +0,0 @@
-package uit.config.core;
-
-
-public class test {
-	public static void main(String args[]){
-		XMLReader rd = new XMLReader();
-		System.out.println(rd.relationWordMapping("write"));
-	}
-}
